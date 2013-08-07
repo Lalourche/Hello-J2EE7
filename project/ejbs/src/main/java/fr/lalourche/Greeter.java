e7e1b937dae1409cda1f6346da1e770a4e2916e4
Lalourche <la_lourche@yahoo.fr>
2013-08-07 12:31:12 +0200

    Moved Greeter into ejbs project.

--- project/ejbs/src/main/java/fr/lalourche/Greeter.java
+++ project/ejbs/src/main/java/fr/lalourche/Greeter.java
@@ -5,10 +5,13 @@ package fr.lalourche;
 
 import java.io.OutputStream;
 
+import javax.ejb.Local;
+
 /**
  * @author Lalourche
  *
  */
+@Local
 public interface Greeter
 {
   public String createGreeting(String name);
